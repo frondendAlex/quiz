25780cf07af7cec3549987d7b3d6472499307729
frondendAlex <alex-moskow@list.ru>
2024-08-04 10:18:04 +0300

    Форма входа

---
+++ admin/admin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+    <!-- ** Font ** -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    <!-- // Font // -->
+
+    <title>Войти</title>
+</head>
+<body>
+
+    <div class="adminform">
+        <form class="adminform__form" action="" method="POST">
+            <h2 class="adminform__title">Войти</h2>
+            <input class="adminform__input" type="text" placeholder="Ваш email">
+            <input class="adminform__input" type="text" placeholder="Пароль">
+            <button class="adminform__btn" typr="submit">Войти</button>
+        </form>
+    </div>
+
+</body>
+</html>
